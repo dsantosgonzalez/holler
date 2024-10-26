94f479388dfb21d9ec3c8dd119beab2d1cf4cd58
jackplo <jackplo53@gmail.com>
2024-10-26 17:43:20 -0400

    Icons based on report type, sidebar, more custom icons

---
+++ holler/src/utils/icons.js
@@ -0,0 +1,37 @@
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+import powerOutage from '../assets/power_outage.png';
+import fire from '../assets/fire.png';
+import block from '../assets/block.png';
+import flood from '../assets/flood.png';
+
+const PowerIcon = L.icon({
+    iconUrl: powerOutage,
+    shadowUrl: iconShadow,
+    iconAnchor: [16, 16],
+    shadowSize: 32
+  });
+  
+  const BlockIcon = L.icon({
+    iconUrl: block,
+    shadowUrl: iconShadow,
+    iconAnchor: [16, 16],
+    shadowSize: 32
+  });
+  
+  const FloodIcon = L.icon({
+    iconUrl: flood,
+    shadowUrl: iconShadow,
+    iconAnchor: [16, 16],
+    shadowSize: 32
+  });
+  
+  const FireIcon = L.icon({
+    iconUrl: fire,
+    shadowUrl: iconShadow,
+    iconAnchor: [16, 16],
+    shadowSize: 32
+  });
+
+export { PowerIcon, BlockIcon, FloodIcon, FireIcon } ;
