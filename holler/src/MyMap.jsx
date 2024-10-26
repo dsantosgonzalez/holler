dc9259dfffeb8da8a55899fbb5979fb8b525ec14
jackplo <jackplo53@gmail.com>
2024-10-26 04:58:27 -0400

    Fixed map styling, added a bounding box, centered the map

--- holler/src/MyMap.jsx
+++ holler/src/MyMap.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import { MapContainer, TileLayer} from 'react-leaflet';  //Marker, Popup
+import { MapContainer, TileLayer } from 'react-leaflet';  //Marker, Popup
+import { LatLngBounds } from 'leaflet';
 
 function MyMap() {
-  const position = [29.6520, -82.3250]; // Latitude and longitude for the initial map center
+  const position = [27.6648, -81.5158]; // Latitude and longitude for the initial map center
+  const bounds =  new LatLngBounds([24.2, -88.0], [31.3, -78.8])
+
 
   return (
-    <MapContainer center={position} zoom={8} style={{ height: "75vh", width: "80vw", margin: "1em auto" }}>
+    <MapContainer center={position} maxBounds={bounds} boundsOptions={{padding: [50, 50]}} maxZoom={24} minZoom={7} zoom={7} style={{ height: "90vh", width: "100vw", margin: "1em auto" }}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
