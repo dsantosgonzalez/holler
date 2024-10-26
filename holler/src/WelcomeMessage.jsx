c0a5acc9b31bf085bb1b17946be760a4d0b263a9
dsantosgonzalez <santosdiegosd@gmail.com>
2024-10-26 01:48:18 -0400

    frontend initial

---
+++ holler/src/WelcomeMessage.jsx
@@ -0,0 +1,18 @@
+import logo from './speaking-head.png';
+import { Link } from 'react-router-dom';
+import "./WelcomeMessage.css";
+
+const WelcomeMessage = () => {
+    return (
+      <div>
+        <h1 className="wm-main-title">holler</h1>
+        <p className="wm-paragraph">a tool for local governments during natural disasters</p>
+        <img src={logo} className="App-logo" alt="logo" />
+        <Link to="/dashboard" style={{ textDecoration: 'none'}}>
+            <button>Access the disaster dashboard</button>
+        </Link>
+      </div>
+    );
+  }
+
+export default WelcomeMessage;
