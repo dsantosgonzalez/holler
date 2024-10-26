b2d17a98b10cb4338c3312b13952757745cae1d3
jackplo <jackplo53@gmail.com>
2024-10-26 12:26:19 -0400

    Refactored and generalized backend

---
+++ backend/models/Hazard.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const HazardSchema = new mongoose.Schema({
+    type: {
+        type: String,
+        required: true,
+        enum: ["Power Outage", "Fire", "Flooding", "Blocked Road", "Debris", "Structural Damage"]
+    },
+    date: { type: String, required: true },
+    severity: {
+        type: String,
+        required: true,
+        enum: ["Critical", "Moderate", "Low"]
+    },
+    longitude: { type: Number, required: true},
+    latitude: { type: Number, required: true},
+    imageUrl: { type: String, required: true }
+}, {collection: "Hazards"})
+
+const Hazard = mongoose.model('Hazard', HazardSchema);
+
+module.exports = Hazard;
