b2b73acfe40e78dcac8b525f6116dbefbe31e5f6
Goran <goran.not@ufl.edu>
2024-10-27 00:01:56 -0400

    Button pressing works just need to link api data

--- holler/src/Components/Dashboard.jsx
+++ holler/src/Components/Dashboard.jsx
@@ -6,7 +6,25 @@ import floodPic from "../assets/flood.png"
 import roadBlocking from "../assets/block.png"
 import firePic from "../assets/fire.png"
 import electricPic from "../assets/power_outage.png"
+
+const floodReport =() =>{
+    console.log("flood report pressed");
+}
+const fireReport =() =>{
+    console.log("fire report pressed");
+}
+const cameraSubmit =() =>{
+    console.log("camera button pressed");
+}
+const powerReport =() =>{
+    console.log("power report pressed");
+}
+const roadReport =() =>{
+    console.log("road report pressed");
+}
+
 const Dashboard = () => {
+    
     return (
         <div>
             <h1 className="dashboard-main-title">Disaster Dashboard</h1>
@@ -21,32 +39,32 @@ const Dashboard = () => {
             <MyMap />
             <div className="buttonBar">
                 
-                <Link to="/showFlood" style = {{textDecoration: 'none'}}>
-                    <button className = "Flood-button">
+                
+                    <button className = "Flood-button" onCLick = {floodReport}>
                     <img src={floodPic} className="button-Icon" alt="flood" />
                     </button>
-                </Link>
-                <Link to="/submitPhoto" style={{textDecoration: 'none'}}>
-                    <button className = "fire-button">
+                
+                
+                    <button className = "fire-button" onClick = {fireReport}>
                     <img src={firePic} className="button-Icon" alt="fire" />
                     </button>
-                </Link>
-                <Link to="/submitPhoto" style={{textDecoration: 'none'}}>
-                    <button className = "Camera-button">
+                
+                
+                    <button className = "Camera-button" onClick = {cameraSubmit}>
                     <img src={cameraPic} className="button-Icon" alt="fire" />
                     </button>
-                </Link>
-                <Link to="/submitPhoto" style={{textDecoration: 'none'}}>
-                    <button className = "power-button">
+               
+                
+                    <button className = "power-button" onClick = {powerReport}>
                     <img src={electricPic} className="button-Icon" alt="power" />
                     </button>
-                </Link>
                 
-                <Link to="/showRoadBlock" style={{textDecoration: 'none'}}>
-                    <button className= "RoadBlock-button">
+                
+                
+                    <button className= "RoadBlock-button" onClick = {roadReport}>
                     <img src={roadBlocking} className="button-Icon" alt="roadblock" />
                     </button>
-                </Link>
+                
                 
             </div>
             {
