9a361240fe9b79f911e622c587f49748fbe7b97f
Goran <goran.not@ufl.edu>
2024-10-26 22:05:18 -0400

    Button bar implemented with buttons, no functionality yet

--- holler/src/Components/Dashboard.jsx
+++ holler/src/Components/Dashboard.jsx
@@ -1,7 +1,11 @@
 import "./Dashboard.css";
 import { Link } from 'react-router-dom';
 import MyMap from "./MyMap";
-
+import cameraPic from "../assets/icons8-camera-48.png"
+import floodPic from "../assets/flood.png"
+import roadBlocking from "../assets/block.png"
+import firePic from "../assets/fire.png"
+import electricPic from "../assets/power_outage.png"
 const Dashboard = () => {
     return (
         <div>
@@ -15,9 +19,40 @@ const Dashboard = () => {
                 </Link>
             </div>
             <MyMap />
+            <div className="buttonBar">
+                
+                <Link to="/showFlood" style = {{textDecoration: 'none'}}>
+                    <button className = "Flood-button">
+                    <img src={floodPic} className="button-Icon" alt="flood" />
+                    </button>
+                </Link>
+                <Link to="/submitPhoto" style={{textDecoration: 'none'}}>
+                    <button className = "fire-button">
+                    <img src={firePic} className="button-Icon" alt="fire" />
+                    </button>
+                </Link>
+                <Link to="/submitPhoto" style={{textDecoration: 'none'}}>
+                    <button className = "Camera-button">
+                    <img src={cameraPic} className="button-Icon" alt="fire" />
+                    </button>
+                </Link>
+                <Link to="/submitPhoto" style={{textDecoration: 'none'}}>
+                    <button className = "power-button">
+                    <img src={electricPic} className="button-Icon" alt="power" />
+                    </button>
+                </Link>
+                
+                <Link to="/showRoadBlock" style={{textDecoration: 'none'}}>
+                    <button className= "RoadBlock-button">
+                    <img src={roadBlocking} className="button-Icon" alt="roadblock" />
+                    </button>
+                </Link>
+                
+            </div>
             {
             // <p className="dashboard-paragraph">TODO: map, image uploads, severity status, update notifications</p>
             }
+           
         </div>
     );
 }
